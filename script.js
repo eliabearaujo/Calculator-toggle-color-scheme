2b7a15393f48d099142f1d5ffe7ceefb7bc2abe8
eliabearaujo <eliabe_trajano@hotmail.com>
2022-08-26 02:15:35 -0300

    Comments

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+//Seleção do display;
 const display = document.querySelector("#display");
+//Seleção de todos os botões, sejam eles numeros ou operadores;
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((item) => {
   item.onclick = () => {
+    //Verifica se o botão clicado é o clear e limpa o display caso true;
     if (item.id == "clear") {
       display.innerText = "";
+      //Verifica se o botão clicado é o backspace, caso true, tranforma o texto do display em string e apaga o ultimo caracter;
     } else if (item.id == "backspace") {
       let string = display.innerText.toString();
       display.innerText = string.substr(0, string.length - 1);
+      //Verifica se o texto do display não é vazio, caso true resolve a equação interna do display;
     } else if (display.innerText != "" && item.id == "equal") {
       display.innerText = eval(display.innerText);
+      // Verifica se o display está vazio e igual foi clicado, caso positivo informa que está vazio, e apaga após 2 segundos.
     } else if (display.innerText == "" && item.id == "equal") {
       display.innerText = "Empty!";
       setTimeout(() => (display.innerText = ""), 2000);
@@ -21,16 +27,10 @@ buttons.forEach((item) => {
 
 const themeToggleBtn = document.querySelector(".color-scheme-toggler");
 const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".toggler-icon");
+
 themeToggleBtn.addEventListener("click", handleTheme);
 
-// function handleTheme() {
-//   calculator.classList.toggle("dark");
-//   themeToggleBtn.classList.toggle("active");
-// }
-// // let isDark = true;
-// // themeToggleBtn.onclick = () => {
-// //   calculator.classList.toggle("dark");
-// //   themeToggleBtn.classList.toggle("active");
-// //   isDark = !isDark;
-// // };
+function handleTheme() {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+}
